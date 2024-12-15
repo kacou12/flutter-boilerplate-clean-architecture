9c3a637b9ccbcb254519506da30c81ac4b98f544
Kacou Yannick <kacou585@gmail.com>
2024-12-15 01:26:22 +0000

    fix: login implementation

--- lib/features/auth/presentation/pages/onboarding.dart
+++ lib/features/auth/presentation/pages/onboarding.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:my/features/auth/presentation/widgets/submit_button.dart';
+import 'package:my/core/widgets/buttons/submit_button.dart';
 
 class OnBoardingPage extends StatefulWidget {
   const OnBoardingPage({super.key});
